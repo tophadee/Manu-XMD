6231eaf23bf3bfcad790b982ab751973d496c1c7
tophadee <hadeedurrehman05@gmail.com>
2025-03-07 16:26:48 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&sLsxTT4D#N2-rTDJgBUMDx5uXUO6MXKXPOxKaEOMZ6sNNMi5-OjQ' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
